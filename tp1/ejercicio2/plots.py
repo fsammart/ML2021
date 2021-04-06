5273f2ef1b9eafc9f0962daee847fc81b0fa4297
marina <mfuster@itba.edu.ar>
2021-04-06 19:56:41 -0300

    :sparkles: ADD: Plots

--- tp1/ejercicio2/plots.py
+++ tp1/ejercicio2/plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 # Even though the name says matrix, this is a dictionary that
@@ -34,5 +35,60 @@ def plot_confusion_matrix(confusion_matrix: dict):
         y_labels[i+1] = categories[i]
     ax.set_yticklabels(y_labels)
 
+    plt.xlabel("Predicción")
+    plt.ylabel("Categoría Real")
+    plt.savefig('confussion_same_probability.png', bbox_inches='tight')
+
     # Shows image
     plt.show()
+
+
+def plot_evaluation_metrics():
+    columns = ['Accuracy', 'Precision', 'Recall', 'F1-Score']
+
+    metrics = pd.read_csv('/home/marina/ML2021/tp1/data/Metrics.csv')
+
+    appearances = metrics.loc[metrics['Method'] == 1]
+    quantity = metrics.loc[metrics['Method'] == 2]
+
+    appearances_means = []
+    quantity_means = []
+    appearances_stds = []
+    quantity_stds = []
+
+    for c in columns:
+        appearances_values = np.array(appearances[c])
+        quantity_values = np.array(quantity[c])
+
+        appearances_means.append(np.mean(appearances_values))
+        appearances_stds.append(np.std(appearances_values))
+
+        quantity_means.append(np.mean(quantity_values))
+        quantity_stds.append(np.std(quantity_values))
+
+    plt.xlabel("Métrica de evaluación")
+    plt.ylabel("Valor")
+    plt.errorbar(
+        columns,
+        appearances_means,
+        appearances_stds,
+        linestyle='None',
+        label='Method Appearances',
+        capsize=5,
+        marker='o'
+    )
+    plt.errorbar(
+        columns,
+        quantity_means,
+        quantity_stds,
+        linestyle='None',
+        label='Method Quantities',
+        capsize=5,
+        marker='o'
+    )
+    plt.legend()
+    plt.savefig('avg.png')
+    plt.show()
+
+
+#plot_evaluation_metrics()
