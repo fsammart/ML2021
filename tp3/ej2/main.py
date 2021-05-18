55c3f87729aa8a52a057587efbdfe7d9c5b129e3
marina <mfuster@itba.edu.ar>
2021-05-18 09:05:42 -0300

    :sparkles: ADD: some plot experiments

--- tp3/ej2/main.py
+++ tp3/ej2/main.py
@@ -1,8 +1,9 @@
 from PIL import Image
 from sklearn import svm
 from sklearn.model_selection import train_test_split
-from utils import get_confusion_matrix, get_precision
+from utils import get_confusion_matrix, get_precision, plot_heatmap, map_color
 import numpy as np
+import cv2
 
 COW = 0
 GRASS = 1
@@ -40,6 +41,8 @@ def run_classifier(c_value, kernel='linear'):
 
     test_confusion = get_confusion_matrix(test_predicted, y_test)
     train_confusion = get_confusion_matrix(train_predicted, y_train)
+    plot_heatmap(test_confusion, f'../confusions/test_c_{c_value}_kernel_{kernel}.png')
+    plot_heatmap(train_confusion, f'../confusions/train_c_{c_value}_kernel_{kernel}.png')
 
     test_precision = get_precision(test_confusion)
     train_precision = get_precision(train_confusion)
@@ -52,14 +55,45 @@ def run_classifier(c_value, kernel='linear'):
     return test_precision, train_precision
 
 
-c_range = np.arange(0.1, 5.2, 0.2)
+def run_c_values():
+    c_range = np.arange(20)
 
-filename = 'results.csv'
-final_string = ''
+    filename = 'c_results_bis.csv'
+    final_string = ''
 
-for c in c_range:
-    test_precision, train_precision = run_classifier(c)
-    final_string += f'{c},linear,{test_precision},{train_precision}\n'
+    for c in c_range:
+        test_precision, train_precision = run_classifier(c)
+        final_string += f'{c},linear,{test_precision},{train_precision}\n'
 
-with open(filename, 'w') as fp:
-    fp.write(final_string)
+
+def run_kernels():
+    kernels = ['linear', 'poly', 'rbf']
+
+    filename = 'kernel_results.csv'
+    final_string = ''
+
+    for ker in kernels:
+        test_precision, train_precision = run_classifier(c_value=1.0, kernel=ker)
+        final_string += f'{1},{ker},{test_precision},{train_precision}\n'
+
+    with open(filename, 'w') as fp:
+        fp.write(final_string)
+
+
+def run_cow(image, c_value=1.0, kernel='linear'):
+    # multiclass support is handled by one-vs-one scheme
+    classifier = svm.SVC(C=c_value, kernel=kernel)
+    classifier.fit(X_train, y_train)
+
+    samples, _ = get_class_data(image, 4)
+    original = np.asarray(Image.open(image))
+
+    predicted = classifier.predict(samples)
+    result_image = np.array(list(map(map_color, predicted))).reshape(original.shape)
+
+    output = np.hstack([original, result_image])
+    im = Image.fromarray(output.astype(np.uint8))
+    im.save('cow_vs_predicted.png')
+
+
+run_cow('../data/cow.jpg')
--- tp3/ej2/utils.py
+++ tp3/ej2/utils.py
@@ -1,8 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 COW = 0
 GRASS = 1
 SKY = 2
+
+COW_COLOR = (219, 149, 156)
+GRASS_COLOR = (61, 130, 69)
+SKY_COLOR = (145, 215, 235)
 
 
 def get_confusion_matrix(predictions, truths):
@@ -17,3 +22,68 @@ def get_precision(confusion_matrix):
     diagonal = np.diagonal(confusion_matrix)
 
     return np.mean(diagonal/sum_columns)
+
+def plot_c_results():
+    # format of data is c_value, kernel=linear, test_precision, train_precision
+    data = np.genfromtxt('c_results.csv', delimiter=',')
+    c_values = []
+    test_precisions = []
+    train_precisions = []
+    for d in data:
+        c_values.append(round(float(d[0]), 2))
+        test_precisions.append(float(d[2]))
+        train_precisions.append(float(d[3]))
+
+    plt.plot(test_precisions, color='magenta', label='test')
+    plt.plot(train_precisions, color='royalblue', label='train')
+    plt.xlabel("Valor C")
+    plt.ylabel("Precisión")
+    plt.title("Precisión SVM vs. C")
+    plt.legend()
+    plt.xticks(ticks=np.arange(len(c_values)), labels=c_values)
+
+    plt.savefig("c_value_test_train.png")
+
+
+def plot_kernel_results():
+    # format of data is c_value, kernel=linear, test_precision, train_precision
+    data = np.genfromtxt('kernel_results.csv', delimiter=',')
+    kernels = ['linear', 'poly', 'rbf']
+    test_precisions = []
+    train_precisions = []
+    for d in data:
+        test_precisions.append(float(d[2]))
+        train_precisions.append(float(d[3]))
+
+    plt.plot(test_precisions, color='magenta', label='test')
+    plt.plot(train_precisions, color='royalblue', label='train')
+    plt.xlabel("Kernel")
+    plt.ylabel("Precisión")
+    plt.title("Precisión SVM vs. Kernel")
+    plt.legend()
+    plt.xticks(ticks=np.arange(len(kernels)), labels=kernels)
+
+    plt.savefig("kernel_test_train.png")
+
+
+def plot_heatmap(matrix, filename):
+    fig, ax = plt.subplots()
+    ax.matshow(matrix, cmap=plt.cm.Blues)
+
+    for i, j in np.ndindex(matrix.shape):
+        c = matrix[i][j]
+        ax.text(i, j, str(c), va='center', ha='center')
+
+    plt.xlabel("Predicción")
+    plt.ylabel("Valor Real")
+    plt.savefig(f'{filename}', bbox_inches='tight')
+    plt.close()
+
+
+def map_color(x):
+    if x == COW:
+        return COW_COLOR
+    if x == GRASS:
+        return GRASS_COLOR
+    if x == SKY:
+        return SKY_COLOR
