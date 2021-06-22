1d44a14cb7e83915a7172697f7a62170424a2dbb
marina <mfuster@itba.edu.ar>
2021-06-22 12:58:04 -0300

    :recycle: REFACTOR: main program methods
    now includes more information

--- tp4/ej2/unsupervised.py
+++ tp4/ej2/unsupervised.py
@@ -1,38 +1,9 @@
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from tp4.ej2.kohonen import Kohonen
-from tp4.ej2.k_means import KMeans, code_method
+from tp4.ej2.k_means import KMeans
 from tp4.ej2.hierarchical_clustering import HierarchicalClustering
-
-# returns pandas df
-# attributes: list of attributes to remove
-# scaler: sklearn scaler to fit and transform data
-def load_data(filename, attributes=None):
-    df = pd.read_csv(filename, delimiter=';')
-    if attributes:
-        df = df[attributes]
-    return df
-
-
-# inplace replaces nan with most frequent value for each column
-def replace_nan(df: pd.DataFrame):
-    nan_counts = []
-    columns = df.columns
-    for col in columns:
-        nan_counts.append((col, df[col].isna().sum()))
-        frequent = df[col].mode()
-        df[col] = df[col].fillna(float(frequent))
-    return nan_counts
-
-def scale_data(df):
-    # TODO: justify why this one
-    scaler = StandardScaler()
-    headers = df.columns
-    np_df = df.values
-    scaled = scaler.fit_transform(np_df)
-    return scaler, pd.DataFrame(scaled, columns=headers)
+from tp4.ej2.preprocessing import load_data, replace_nan, scale_data
 
 
 def hierarchical_clustering_program(train, test, train_y, test_y):
@@ -61,34 +32,47 @@ def k_means_program(train, test, train_y, test_y):
     # TODO: metrics to evaluate
 
 
-# ************ MAIN PROGRAM ************ #
+def kohonen_program(train, test, train_y, test_y):
+    # feats = 1
+    # k = 10
+    # eta = 0.1
+    # epochs = 100
+    # data, y = make_blobs(n_samples=1000, n_features=feats, centers=3)
+    # som = Kohonen(k, k, eta, feats, data, rand_weights=False)
+    # som.train(epochs)
+    # som.u_matrix('u_matrix.png')
+    # som.activations('activations.png')
+    pass
 
-attributes = ['sex', 'age', 'cad.dur', 'choleste']
-label = ['sigdz']
 
-dataframe = load_data('../data/acath.csv', attributes + label)
-info = replace_nan(dataframe)
+def run_program(kohonen=False, k_means=False, hc=False, balance=None):
+    attributes = ['sex', 'age', 'cad.dur', 'choleste']
+    label = ['sigdz']
 
-print('Nan values per column in original dataframe')
-print(info)
+    dataframe, dataframe_info = load_data('../data/acath.csv', attributes + label, balance=balance)
+    #print(dataframe_info)
 
-data = dataframe[attributes]
-labels = dataframe[label]
-std_scaler, data = scale_data(data)
+    replacement_info = replace_nan(dataframe)
+    #print('Nan values per column in original dataframe')
+    #print(replacement_info)
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.7, random_state=42) #TODO: change 0.9
-print(f'{len(x_train)} train records and {len(x_test)} test records')
+    data = dataframe[attributes]
+    labels = dataframe[label]
+    std_scaler, data = scale_data(data)
 
-hierarchical_clustering_program(x_train, x_test, y_train, y_test)
-k_means_program(x_train, x_test, y_train, y_test)
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.3, random_state=42) #TODO: change 0.9
+    print(f'{len(x_train)} train records and {len(x_test)} test records')
 
-# feats = 1
-# k = 10
-# eta = 0.1
-# epochs = 100
-# data, y = make_blobs(n_samples=1000, n_features=feats, centers=3)
-# som = Kohonen(k, k, eta, feats, data, rand_weights=False)
-# som.train(epochs)
-# som.u_matrix('u_matrix.png')
-# som.activations('activations.png')
+    if hc:
+        hierarchical_clustering_program(x_train, x_test, y_train, y_test)
+    if k_means:
+        k_means_program(x_train, x_test, y_train, y_test)
+    if kohonen:
+        kohonen_program(x_train, x_test, y_train, y_test)
 
+    print('\n')
+
+
+if __name__ == '__main__':
+    run_program()
+    run_program(balance='sigdz')
